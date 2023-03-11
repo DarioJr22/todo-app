3c0db107b9ea825471bb96c4dcff7d9446a95e65
Dário <dariojr1233@gmail.com>
2023-03-11 20:47:46 -0300

    Algumas modificações no CRUD.

--- src/app/todo-list/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list/todo-list.component.ts
@@ -9,9 +9,32 @@ import{ CdkDragDrop, moveItemInArray }from '@angular/cdk/drag-drop'
 })
 export class TodoListComponent {
 
-  constructor(private changeDetectorRef: ChangeDetectorRef){
+  constructor(private cdr: ChangeDetectorRef){
 
   }
+  //Coisas para estudar e documentar
+  /*
+  --------------Estudar-----------------------
+  - [ ] Uso do CDK drag and drop
+  - [ ] Uso de view Childs
+  - [ ] aot
+  - [ ] Funcionamento de modules com angular
+  --------------PENDENCIAS---------------------
+  - [ ] Resolver o Update
+  - [ ] Resolver o Remove
+  - [ ] Resolver o All COMPLEET
+  - [ ] Resolver o All COMPLEET
+  - [ ] Resolver o Clear Compleet
+  - [ ] Resolver o [Quando estiver marcado, grifar a tarefa]
+
+  - [ ] Entender como fazer o modo dark / Fazer
+  - [ ] Entender como Estilizar o check box / Fazer
+  - [ ] Entender como plotar esse fundo metade metade / Fazer
+
+
+
+  */
+
   taskList:Task[] = []
 
 
@@ -19,9 +42,8 @@ export class TodoListComponent {
 
   drop(event:CdkDragDrop<any[]>){
     console.log(event);
-
     moveItemInArray(this.taskList,event.previousIndex,event.currentIndex)
-    this.changeDetectorRef.markForCheck();
+    this.cdr.markForCheck();
   }
 
   //Task List operations
@@ -40,21 +62,55 @@ export class TodoListComponent {
 
   removeTask(id:number){
    let deleteItem:any =  this.taskList.find((a)=>a.id === id)
-   this.taskList.slice(this.taskList.indexOf(deleteItem),1)
+   this.taskList.splice(this.taskList.indexOf(deleteItem),1)
   }
 
-  updateTask(){
+  updateTask(id:number,updateT:any,check?:boolean){
+    //Item á ser atualizado
+    let updateitem:any = this.taskList.find((a)=> a.id === id)
+
+    //Se for uma atualização do CheckBox
+    if (check) {
+      let update:Task = {
+        id:id,
+        checked:!updateT,
+        description:updateitem.description
+      }
+      this.taskList.splice(this.taskList.indexOf(updateitem),1,update)
+    }else{
+      let update:Task = {
+        id:id,
+        checked:updateitem.checked,
+        description:updateT
+      }
+      this.taskList.splice(this.taskList.indexOf(updateitem),1,update)
+    }
+
+    //Update the number of taks completed
+    this.numberTaskCompleted()
+    this.cdr.detectChanges()
 
   }
 
-  activeALL(){
+  activeAll(){
+    this.taskList.forEach((t)=> !t.checked ? t.checked = true : t.checked = t.checked)
+    //Update the number of taks completed
+    this.numberTaskCompleted()
 
   }
 
   removeAllCompleted(){
-
+    this.taskList.forEach((t)=> t.checked ? this.removeTask(t.id) : t.checked = t.checked)
+    //Update the number of taks completed
+    this.numberTaskCompleted()
   }
 
+  numberTaskCompleted(){
+    let numberOfTasksCompleted =  this.taskList.filter((t)=> t.checked == true)
+    return numberOfTasksCompleted.length
+  }
+
+  //Atualiza o elemento manipulado no ts para o html
 
 
 
